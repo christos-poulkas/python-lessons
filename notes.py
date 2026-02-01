0620b2ffe884adf9cd8b59008f368dd863241c5f
Fritzalas <144057272+Fritzalas@users.noreply.github.com>
2026-02-01 15:43:53 +0200

    Some notes

---
+++ notes.py
@@ -0,0 +1,8 @@
+#01/02/2026
+
+######Types of variables##########
+#integers (int) -> akeraioi 1,2,3,4,5,-1,-2,-3
+#float ->   1.2,4.3,-5.67
+#strings -> "Kosmas3io23480948329048320943290"
+#char -> 'a','2','e'
+#Boolean -> True, False -> xrisi tous einai gia logikes synthikes
