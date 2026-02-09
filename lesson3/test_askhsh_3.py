2ce5a1e227b151341b26720bfdf6c905034f8ec8
chris <c.poulkas@outlook.com>
2026-02-09 21:41:00 +0200

    Μάθημα 3 end-session push

--- lesson3/test_askhsh_3.py
+++ lesson3/test_askhsh_3.py
@@ -16,4 +16,32 @@ def test_askhsh1 ():
         z.remove(100) #-> [2,3,4]
     print(z)
 
-test_askhsh1()
+
+#######selida 7, dialeksh 3 LATHOS H POP######
+
+#askhsh2: zhtav apo ton xrhsh n mathhmata vathmologia > dvse mathhma kai vathmologia
+
+def test_askhsh2 ():
+    archeio = {}
+    q_math = int(input("posa mathhmata?:"))
+    for i in range (q_math):
+        titlos = input("vale mathhma:")
+        vathmos = int(input("vale vathmo:"))
+        archeio[titlos] = vathmos
+    print(archeio)
+    vathmoi_list=archeio.values()
+    print(vathmoi_list)
+    new_list = list(vathmoi_list)
+    print(new_list)
+
+    local_max = 8
+    for i in new_list:
+        if i > local_max:
+            print("kalos mathhths")
+            return 0
+    print("kakos mathhths")
+#ean kapoios apo tous vathmous >8, na kanv print kalos h kakos
+
+
+
+test_askhsh2()
