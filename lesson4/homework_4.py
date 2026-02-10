548eaf02ab2595600006629128d2b760f6af8fcf
chris <c.poulkas@outlook.com>
2026-02-10 21:28:01 +0200

    Μάθημα 4 end-session push

---
+++ lesson4/homework_4.py
@@ -0,0 +1,65 @@
+students = {
+    101: {
+        "name": "Maria",
+        "lessons": {
+            "python": 8,
+            "math": 9,
+            "databases": 7
+        },
+        "active": True,
+        "year": 2
+    },
+    102: {
+        "name": "Nikos",
+        "lessons": {
+            "python": 6,
+            "math": 5,
+            "databases": 6
+        },
+        "active": False,
+        "year": 1
+    },
+    103: {
+        "name": "Eleni",
+        "lessons": {
+            "python": 9,
+            "math": 8,
+            "databases": 9
+        },
+        "active": True,
+        "year": 3
+    },
+    104: {
+        "name": "Giorgos",
+        "lessons": {
+            "python": 7,
+            "math": 6,
+            "databases": 5
+        },
+        "active": True,
+        "year": 2
+    },
+    105: {
+        "name": "Anna",
+        "lessons": {
+            "python": 10,
+            "math": 9,
+            "databases": 8
+        },
+        "active": True,
+        "year": 4
+    },
+    106: {
+        "name": "Kostas",
+        "lessons": {
+            "python": 5,
+            "math": 6,
+            "databases": 4
+        },
+        "active": False,
+        "year": 1
+    }
+}
+
+#1. ftiakse ena dictionary me onoma tou mathhth kai th vathmologia tou sta mathhmata (key: onoma kai value: vathmologia)
+#2. ftiakse ena dictionary me onoma tou mathhth kai th mesh vathmologia tou se ola ta mathhamata
---
+++ lesson4/test_askhsh_4.py
@@ -0,0 +1,100 @@
+#def , print avg mathhmatvn + print mathhmata >=8
+
+def askhsh1():
+    grades = { #list(grades.keys()) -> ["Python","Finance","Statistics"], list(grades.values()) -> [8, 7, 9]
+        "Python": 8,
+        "Finance": 7,
+        "Statistics": 9
+    }
+
+    vathmoi = list(grades.values())
+    vathmoi_avg = sum(vathmoi) / len(vathmoi)
+    print("mesos oros:", vathmoi_avg)
+
+    mathhmata = list(grades.keys())
+    #grades["Python"] -> 8
+    for i in mathhmata:
+        if grades[i] >= 8:
+            print (i)
+
+
+
+#na ektypvsv ton mathhth me ton megalytero vathmo
+def askhsh2():
+    students = {
+        "Anna": 9,
+        "Nikos": 6,
+        "Maria": 1,
+        "Giorgos": 7,
+        "Eleni": 9
+    }
+    mathhtes = list(students.keys())
+    vathmoi = list(students.values())
+    local_max = -1
+    for i in mathhtes:
+        if students[i] >= local_max:
+            local_max = students[i]
+    for i in mathhtes:
+        if students[i] == local_max:
+            print (i)
+    freq_ex = 0
+    for i in mathhtes:
+        if students[i] >= 8:
+            freq_ex += 1
+    print (freq_ex)
+    freq_kalos = 0
+    for i in mathhtes:
+        if students[i] >= 5 and students[i] <= 7:
+            freq_kalos += 1
+    print (freq_kalos)
+
+    freq_meh = 0
+    for i in mathhtes:
+        if students[i] < 5:
+            freq_meh +=1
+    print (freq_meh)
+
+#3 listes
+
+    list_ex = []
+    list_kalos = []
+    list_meh = []
+    for i in mathhtes:
+        if students[i] >= 8:
+           list_ex.append(i)
+        elif students[i] >= 5 and students[i] <= 7:
+            list_kalos.append(i)
+        elif students[i] < 5:
+            list_meh.append(i)
+
+    final_dict = {
+        "excellent": list_ex,
+        "kalos": list_kalos,
+        "meh": list_meh
+    }
+    print(final_dict)
+
+def askhsh3():
+    students = {
+        101: {"name": "Maria", "python": 8, "active": True},
+        102: {"name": "Nikos", "python": 10, "active": False},
+        103: {"name": "Eleni", "python": 9, "active": True}
+    }
+    initial_list = list(students.values())
+    print(initial_list)
+    for i in initial_list: #to i einai to dictionary
+        if i["active"] == True:
+            print(i["name"])
+
+    # print onomata foithtvn poy einai aktive kai exoyn vathmo > 8
+    print("/////////////")
+    for i in initial_list:
+        if i["active"] == True and i["python"] > 8:
+            print(i["name"])
+
+
+
+
+# grades["Python"] -> 8
+
+askhsh3()
